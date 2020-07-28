c2db3badf123b87c12fe450d4f5758d05f2bd772
1Dimitri <git.dimitri@janczak.net>
2020-07-28 08:28:20 +0200

    added license file removal at the end typos corrected

--- WixSharp SysInternals/Program.cs
+++ WixSharp SysInternals/Program.cs
@@ -26,33 +26,34 @@ namespace WixSharp_SysInternals
             Console.WriteLine("Extracting File \"{0}\" to \"{1}\" .......\n\n", zipFile, zipDir);
               ZipFile.ExtractToDirectory(zipFile, zipDir);
 
-            // Newest file
+            // Newest file to create a version number from
             System.IO.DirectoryInfo directory = new System.IO.DirectoryInfo(zipDir);
             System.IO.FileInfo newestFile = directory.GetFiles().OrderByDescending(f => f.LastWriteTime).First();
             DateTime dtFile = newestFile.LastWriteTimeUtc;
-            String versionStr = dtFile.ToString("yy.MM.dd");
+            String versionStr = dtFile.ToString("yy.MM.dd"); // 0-256 for each version component
             Console.WriteLine($"Version extracted from newest file:{versionStr}");
             Version sysIntVersion = new Version(versionStr);
 
-            // LicenceFile
+            // LicenceFile should be .rtf
             string LicFile = System.IO.Path.Combine(System.IO.Path.GetTempPath(),
                                                     System.IO.Path.GetFileNameWithoutExtension(System.IO.Path.GetRandomFileName())+".rtf");
             string licRtf = @"{\rtf1\ansi\deff0 {\fonttbl {\f0 Times New Roman;}}\f0\fs30 This is not a Microsoft software and it comes with no warranty}";
             System.IO.File.WriteAllText(LicFile, licRtf);
-            // REpack
 
+            // and now Repack files as msi
             var project = new Project()
             {
                 Name = "SysInternalsSuite",
                 Version = sysIntVersion,
                 LicenceFile = LicFile,
+                // Let's display a licence
                 // UI = WUI.WixUI_ProgressOnly,
                 OutFileName = $"SysInternals_{sysIntVersion}",
                 SourceBaseDir = zipDir,
                 ControlPanelInfo = new ProductInfo()
                 {
                     Manufacturer = "NotMicrosoft",
-                    UrlInfoAbout = "https://github.com/1Dimitri/WixSharp_SysInternals"
+                    UrlInfoAbout = "https://github.com/1Dimitri/WixSharpSysInternals"
                 },
                 Dirs = new[]
                 {
@@ -64,15 +65,12 @@ namespace WixSharp_SysInternals
 
             project.GUID = new Guid("6fe30b47-2577-43ad-9095-1861ba25889b");
 
-
-
-            //project.SourceBaseDir = "<input dir path>";
-            //project.OutDir = "<output dir path>";
-
             project.BuildMsi();
 
+            // clean-up temporary files
             System.IO.File.Delete(zipFile);
             System.IO.Directory.Delete(zipDir, true);
+            System.IO.File.Delete(LicFile);
 
         }
 
